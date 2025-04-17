d84097542a3bbef1914eac6ec2dcd69266948b4d
Mikael Sundell <msundell@rapidimages.se>
2025-04-18 00:13:36 +0200

    Changed version to 1.4.0 Improved doubleslider and intslider with input fields not just labels

--- sources/optionswidget.cpp
+++ sources/optionswidget.cpp
@@ -22,21 +22,31 @@ public:
     OptionsWidgetPrivate();
     void init();
     void update();
+    QLineEdit* doubleedit(const QSharedPointer<Option>& option, QWidget* parent = nullptr);
+    QLineEdit* intedit(const QSharedPointer<Option>& option, QWidget* parent = nullptr);
 
 public Q_SLOTS:
     void valueChanged(const QString& id, const QVariant& value);
 
 public:
+    QFont font;
+    int editprecision;
+    int labelprecision;
     QSharedPointer<Preset> preset;
     QPointer<OptionsWidget> widget;
 };
 
-OptionsWidgetPrivate::OptionsWidgetPrivate() {}
+OptionsWidgetPrivate::OptionsWidgetPrivate()
+    : editprecision(6)
+    , labelprecision(3)
+{}
 
 void
 OptionsWidgetPrivate::init()
 {
     preset.reset(new Preset());
+    // font 
+    font.setPointSize(10);
 }
 
 void
@@ -52,12 +62,8 @@ OptionsWidgetPrivate::update()
         labellayout->setSpacing(4);
 
         QLabel* label = new QLabel(option->name, labelwidget);
-        QFont font;
-        font.setPointSize(10);
         label->setFont(font);
         labellayout->addWidget(label);
-        int editprecision = 6;
-        int labelprecision = 3;
         int rowheight = 30;
 
         if (!option->description.isEmpty()) {
@@ -120,51 +126,7 @@ OptionsWidgetPrivate::update()
             QVBoxLayout* optionlayout = new QVBoxLayout(optionwidget);
             optionlayout->setContentsMargins(margins);
 
-            QString value = utils::formatDouble(option->value.toDouble());
-            QLineEdit* lineedit = new QLineEdit(value, optionwidget);
-
-            QDoubleValidator* validator = new QDoubleValidator(option->minimum.toDouble(), option->maximum.toDouble(),
-                                                               editprecision, lineedit);
-            validator->setNotation(QDoubleValidator::StandardNotation);
-            validator->setLocale(QLocale::C);  // use '.' instead of ','
-            lineedit->setValidator(validator);
-            lineedit->setFont(font);
-            lineedit->setCursorPosition(0);
-
-            connect(lineedit, &QLineEdit::textEdited, this, [this, lineedit, option]() {
-                QString text = lineedit->text();
-                if (text.contains(',')) {  // needed, will still allow ','
-                    text.replace(',', '.');
-                    lineedit->setText(text);
-                }
-                if (!(!text.isEmpty() && text.back() == QChar('.'))) {
-                    bool valid;
-                    double value = text.toDouble(&valid);
-                    if (valid) {
-                        double minimum = option->minimum.toDouble();
-                        double maximum = option->maximum.toDouble();
-                        if (value < minimum) {
-                            value = minimum;
-                            lineedit->setText(QString::number(value));
-                        }
-                        else if (value > maximum) {
-                            value = maximum;
-                            lineedit->setText(QString::number(value));
-                        }
-                        valueChanged(option->id, value);
-                    }
-                }
-            });
-
-            connect(lineedit, &QLineEdit::editingFinished, this, [lineedit]() {
-                QString text = lineedit->text();
-                bool valid;
-                double value = text.toDouble(&valid);
-                if (valid) {
-                    lineedit->setText(utils::formatDouble(value));
-                }
-            });
-
+            QLineEdit* lineedit = doubleedit(option, optionwidget);
             optionlayout->addWidget(lineedit);
 
             if (!option->toggle.isEmpty()) {
@@ -239,20 +201,27 @@ OptionsWidgetPrivate::update()
             slider->setRange(0, steps);
             slider->setValue(sliderpos);
 
-            QLabel* sliderlabel = new QLabel(utils::formatDouble(value, labelprecision), widget.data());
-            sliderlabel->setFont(font);
-            sliderlabel->setFixedWidth(60);
-            sliderlabel->setAlignment(Qt::AlignRight | Qt::AlignVCenter);
+            QLineEdit* lineedit = doubleedit(option, widget.data());
+            lineedit->setFixedWidth(60);
 
-            connect(slider, &QSlider::valueChanged, this,
-                    [this, option, sliderlabel, minvalue, maxvalue, steps, labelprecision](int pos) {
-                        double mappedValue = minvalue + ((maxvalue - minvalue) * pos / steps);
-                        sliderlabel->setText(utils::formatDouble(mappedValue, labelprecision));
-                        valueChanged(option->id, mappedValue);
-                    });
+            connect(slider, &QSlider::valueChanged, this, [=]() {
+                double mappedValue = minvalue + ((maxvalue - minvalue) * slider->value() / steps);
+                lineedit->setText(utils::formatDouble(mappedValue));
+                valueChanged(option->id, mappedValue);
+            });
+
+            connect(lineedit, &QLineEdit::editingFinished, this, [=]() {
+                bool valid;
+                double typedValue = lineedit->text().toDouble(&valid);
+                if (valid) {
+                    typedValue = std::clamp(typedValue, minvalue, maxvalue);
+                    int newSliderPos = static_cast<int>(((typedValue - minvalue) / (maxvalue - minvalue)) * steps);
+                    slider->setValue(newSliderPos);
+                }
+            });
 
             sliderlayout->addWidget(slider);
-            sliderlayout->addWidget(sliderlabel);
+            sliderlayout->addWidget(lineedit);
             optionlayout->addLayout(sliderlayout);
 
             if (!option->toggle.isEmpty()) {
@@ -260,10 +229,10 @@ OptionsWidgetPrivate::update()
                 togglebox->setFont(font);
                 togglebox->setChecked(option->enabled);
                 slider->setEnabled(option->enabled);
-                sliderlabel->setEnabled(option->enabled);
-                connect(togglebox, &QCheckBox::toggled, this, [slider, sliderlabel, option](bool checked) {
+                lineedit->setEnabled(option->enabled);
+                connect(togglebox, &QCheckBox::toggled, this, [=](bool checked) {
                     slider->setEnabled(checked);
-                    sliderlabel->setEnabled(checked);
+                    lineedit->setEnabled(checked);
                     option->enabled = checked;
                 });
                 optionlayout->addWidget(togglebox);
@@ -322,30 +291,7 @@ OptionsWidgetPrivate::update()
             QVBoxLayout* optionlayout = new QVBoxLayout(optionwidget);
             optionlayout->setContentsMargins(margins);
 
-            int value = static_cast<int>(option->value.toDouble());  // needed toInt() does not handle decimals
-            QLineEdit* lineedit = new QLineEdit(QString::number(value), optionwidget);
-            QIntValidator* validator = new QIntValidator(option->minimum.toInt(), option->maximum.toInt(), lineedit);
-            lineedit->setValidator(validator);
-            lineedit->setFont(font);
-
-            connect(lineedit, &QLineEdit::textEdited, this, [this, lineedit, option]() {
-                QString text = lineedit->text();
-                bool valid;
-                int value = text.toInt(&valid);
-                if (valid) {
-                    int minimum = option->minimum.toInt();
-                    int maximum = option->maximum.toInt();
-                    if (value < minimum) {
-                        value = minimum;
-                    }
-                    else if (value > maximum) {
-                        value = maximum;
-                    }
-                    lineedit->setText(QString::number(value));
-                    valueChanged(option->id, value);
-                }
-            });
-
+            QLineEdit* lineedit = intedit(option, optionwidget);
             optionlayout->addWidget(lineedit);
 
             if (!option->toggle.isEmpty()) {
@@ -370,20 +316,32 @@ OptionsWidgetPrivate::update()
 
             QHBoxLayout* sliderlayout = new QHBoxLayout();
             QSlider* slider = new QSlider(Qt::Horizontal, widget.data());
-            slider->setRange(option->minimum.toInt(), option->maximum.toInt());
-            slider->setValue(option->value.toInt());
 
-            QLabel* sliderlabel = new QLabel(QString::number(slider->value()), widget.data());
-            sliderlabel->setFont(font);
-            sliderlabel->setFixedWidth(20);
-            sliderlabel->setAlignment(Qt::AlignRight | Qt::AlignVCenter);
+            int minvalue = option->minimum.toInt();
+            int maxvalue = option->maximum.toInt();
+            int value = option->value.toInt();
 
-            connect(slider, &QSlider::valueChanged, this, [this, option, sliderlabel](int value) {
-                sliderlabel->setText(QString::number(value));
-                valueChanged(option->id, value);
-            });
+            slider->setRange(minvalue, maxvalue);
+            slider->setValue(value);
+
+            QLineEdit* lineedit = intedit(option, widget.data());
+            lineedit->setFixedWidth(60);
             sliderlayout->addWidget(slider);
-            sliderlayout->addWidget(sliderlabel);
+            sliderlayout->addWidget(lineedit);
+
+            connect(slider, &QSlider::valueChanged, this, [=](int newValue) {
+                lineedit->setText(QString::number(newValue));
+                valueChanged(option->id, newValue);
+            });
+
+            connect(lineedit, &QLineEdit::editingFinished, this, [=]() {
+                bool valid;
+                int typedValue = lineedit->text().toInt(&valid);
+                if (valid) {
+                    typedValue = std::clamp(typedValue, minvalue, maxvalue);
+                    slider->setValue(typedValue);
+                }
+            });
 
             optionlayout->addLayout(sliderlayout);
 
@@ -392,10 +350,10 @@ OptionsWidgetPrivate::update()
                 togglebox->setFont(font);
                 togglebox->setChecked(option->enabled);
                 slider->setEnabled(option->enabled);
-                sliderlabel->setEnabled(option->enabled);
-                connect(togglebox, &QCheckBox::toggled, this, [slider, sliderlabel, option](bool checked) {
+                lineedit->setEnabled(option->enabled);
+                connect(togglebox, &QCheckBox::toggled, this, [=](bool checked) {
                     slider->setEnabled(checked);
-                    sliderlabel->setEnabled(checked);
+                    lineedit->setEnabled(checked);
                     option->enabled = checked;
                 });
                 optionlayout->addWidget(togglebox);
@@ -460,6 +418,84 @@ OptionsWidgetPrivate::update()
     layout->setColumnStretch(1, 3);
     layout->addItem(new QSpacerItem(0, 0, QSizePolicy::Minimum, QSizePolicy::Expanding), row, 0, 1, 2);
     widget->setLayout(layout);
+}
+
+QLineEdit*
+OptionsWidgetPrivate::doubleedit(const QSharedPointer<Option>& option, QWidget* parent)
+{
+    QString value = utils::formatDouble(option->value.toDouble());
+    QLineEdit* lineedit = new QLineEdit(value, parent);
+    QDoubleValidator* validator = new QDoubleValidator(option->minimum.toDouble(), option->maximum.toDouble(),
+                                                       editprecision, lineedit);
+    validator->setNotation(QDoubleValidator::StandardNotation);
+    validator->setLocale(QLocale::C);  // use '.' instead of ','
+    lineedit->setValidator(validator);
+    lineedit->setFont(font);
+    lineedit->setCursorPosition(0);
+
+    connect(lineedit, &QLineEdit::textEdited, this, [this, lineedit, option]() {
+        QString text = lineedit->text();
+        if (text.contains(',')) {  // needed, will still allow ','
+            text.replace(',', '.');
+            lineedit->setText(text);
+        }
+        if (!(!text.isEmpty() && text.back() == QChar('.'))) {
+            bool valid;
+            double value = text.toDouble(&valid);
+            if (valid) {
+                double minimum = option->minimum.toDouble();
+                double maximum = option->maximum.toDouble();
+                if (value < minimum) {
+                    value = minimum;
+                    lineedit->setText(QString::number(value));
+                }
+                else if (value > maximum) {
+                    value = maximum;
+                    lineedit->setText(QString::number(value));
+                }
+                valueChanged(option->id, value);
+            }
+        }
+    });
+
+    connect(lineedit, &QLineEdit::editingFinished, this, [lineedit]() {
+        QString text = lineedit->text();
+        bool valid;
+        double value = text.toDouble(&valid);
+        if (valid) {
+            lineedit->setText(utils::formatDouble(value));
+        }
+    });
+    return lineedit;
+}
+
+QLineEdit*
+OptionsWidgetPrivate::intedit(const QSharedPointer<Option>& option, QWidget* parent)
+{
+    int value = static_cast<int>(option->value.toDouble());  // needed toInt() does not handle decimals
+    QLineEdit* lineedit = new QLineEdit(QString::number(value), parent);
+    QIntValidator* validator = new QIntValidator(option->minimum.toInt(), option->maximum.toInt(), lineedit);
+    lineedit->setValidator(validator);
+    lineedit->setFont(font);
+
+    connect(lineedit, &QLineEdit::textEdited, this, [this, lineedit, option]() {
+        QString text = lineedit->text();
+        bool valid;
+        int value = text.toInt(&valid);
+        if (valid) {
+            int minimum = option->minimum.toInt();
+            int maximum = option->maximum.toInt();
+            if (value < minimum) {
+                value = minimum;
+            }
+            else if (value > maximum) {
+                value = maximum;
+            }
+            lineedit->setText(QString::number(value));
+            valueChanged(option->id, value);
+        }
+    });
+    return lineedit;
 }
 
 void
